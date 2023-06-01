3dce166fb2c87592cad45a8092f8b9f8750b45c6
rob-rp <robert.pasols@yahoo.com>
2023-06-01 19:19:55 -0400

    Fixed bash and pwd files with if statement

--- bash.js
+++ bash.js
@@ -1,15 +1,20 @@
-const pwd = require('./pwd');
+// const pwd = require('./pwd');
 // pwd();
 // // console.log(pwd());
 
-console.log(process.cwd());// use an if statement to see what gets typed in so we can use process.cwd() to print path
+//console.log(process.cwd());// use an if statement to see what gets typed in so we can use process.cwd() to print path
 
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); //remove the newline
-
-  process.stdout.write('You typed: ' + cmd + process.cwd());
+  if( cmd === 'pwd'){
+    const pwd = require('./pwd');
+    process.stdout.write('prompt > ');
+  }
+  else {
+  process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');
+  }
 });
--- pwd.js
+++ pwd.js
@@ -11,4 +11,4 @@
 // });
 // }
 
-//module.exports = thePWD;
+module.exports = console.log(process.cwd());
